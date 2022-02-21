d73786b79f7ebd435294e8827bd133ee5f31d770
Lisi <lisandro.anexa@gmail.com>
2022-02-21 20:58:28 -0300

    Basic sleeping script

---
+++ Assets/Scripts/InteractBed.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractBed : MonoBehaviour
+{
+    public GameObject PressFText;
+    public Image blackScreen;
+    public GameObject sleepingCamera;
+    bool playerIsColliding = false;
+    bool sleeping = false;
+    float sleepTimer = 0f;
+
+    void Update()
+    {
+        PressFText.SetActive(playerIsColliding);
+        if(playerIsColliding && Input.GetKeyDown(KeyCode.F) && !sleeping)
+        {
+            // Do Something...
+            sleeping = true;
+            playerIsColliding = false;
+            GameObject.FindGameObjectWithTag("Player").SetActive(false);
+            sleepingCamera.SetActive(true);
+        }
+
+        if(sleeping)
+        {
+            sleepTimer += Time.deltaTime/3;
+            blackScreen.color = new Color(0, 0, 0, sleepTimer);
+            if(sleepTimer > 1)
+            {
+                // Next Scene
+                scenesManagement.scene++;
+            }
+        }
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        playerIsColliding = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        playerIsColliding = false;
+    }
+
+}
